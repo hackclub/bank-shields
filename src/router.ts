91f2353e81a43904f5334f3b1a4de321823538a5
Gary Tou <gary@garytou.com>
2022-07-01 08:55:06 -0700

    Properly format money

--- src/router.ts
+++ src/router.ts
@@ -46,7 +46,7 @@ function formatMoney(amount_cents: number) {
 		//minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
 		//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 	});
-	return formatter.format(amount_cents);
+	return formatter.format(amount_cents / 100);
 }
 
 function bankLogo() {
